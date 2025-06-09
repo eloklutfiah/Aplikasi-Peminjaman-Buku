dac96cf880482151a00c56e0df63c60a228f1447
eloklutfiah <elokfaizatul650@gmail.com>
2025-06-09 20:55:41 +0700

    Menambah/membuat form login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,4 +1,4 @@
-  <div class="container">
+<div class="container">
 
     <!-- Outer Row -->
     <div class="row justify-content-center">
@@ -9,43 +9,26 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Halaman Login</h1>
                   </div>
-                  <form class="user">
+                  <?= $this->session->flashdata('pesan'); ?>
+                  <form class="user" method="post" action="<? base_url('autentifikasi'); ?>">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" value="<?= set_value('email'); ?>" id="email" placeholder="Masukkan Alamat Email" name="email">
+                      <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    <input type="password" class="form-control form-control-user" id="password" placeholder="password" name="password"> 
+                    <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?> 
                     </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
+                    <button type="submit" class="btn-primary btn-user btn-block"> Masuk </button>
                   </form>
                   <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
+                  <div class="text-center"> <a class="small" href="<?= base_url('authentifikasi/lupaPassword'); ?>">Lupa Password?</a> </div>
+                  <div class="text-center"> <a class="small" href="<?= base_url('authentifikasi/registrasi'); ?>">Daftar Member!</a> </div>
                 </div>
               </div>
             </div>
@@ -56,6 +39,4 @@
 
     </div>
 
-  </div>
-
-  
+</div>
