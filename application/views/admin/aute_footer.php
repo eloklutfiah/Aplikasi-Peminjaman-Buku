49d3b6a35ad5259f1ba4a5fe6a56ee3b84818ba2
eloklutfiah <elokfaizatul650@gmail.com>
2025-06-14 10:21:08 +0700

    Update file layout admin dan login

--- application/views/admin/aute_footer.php
+++ application/views/admin/aute_footer.php
@@ -7,7 +7,7 @@
   
   <!-- Custom scripts for all pages--> 
   <script src="<?= base_url('assets/'); ?>vendor/js/sb-admin-2.min.js"></script> 
-  <script> $('.alert-message').alert().delay(3000).slideUp('slow'); </script> 
+  <script> $('.alert-message').alert().delay(3000).slideUp('slow'); </script>
 </body> 
 
 </html>
